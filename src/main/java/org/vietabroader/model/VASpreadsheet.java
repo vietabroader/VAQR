f32cdd0560233e077624c48fc320f2d255d2021b
thucnhi2411 <let@lafayette.edu>
2017-06-17 15:10:40 +0700

    Add refreshColumn method

--- src/main/java/org/vietabroader/model/VASpreadsheet.java
+++ src/main/java/org/vietabroader/model/VASpreadsheet.java
@@ -2,11 +2,13 @@ package org.vietabroader.model;
 
 import com.google.api.services.sheets.v4.model.Sheet;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import org.vietabroader.GoogleAPIUtils;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -15,8 +17,61 @@ import java.util.List;
  */
 public class VASpreadsheet {
 
+
     private String spreadsheetId;
     private Spreadsheet spreadSheet;
+    private HashMap<String,String> columnMap;
+    private HashMap<String,List<List<Object>> > contentMap;
+
+    public void init(){
+        columnMap = new HashMap<String, String>();
+        contentMap = new HashMap<String,List<List<Object>>>();
+    }
+
+
+    /**
+     * Read first column character from the sheet/spreadsheet for testing the refreshColumn method
+     * @return The Item column character in the specific row
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    public String readFirstColumn() throws IOException, GeneralSecurityException{
+        refreshColumn("A");
+        return columnMap.get("Item");
+    }
+
+    /**
+     * Read first key from the Item column for testing the refreshColumn method
+     * @return The first key
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    public String readFirstKey() throws IOException, GeneralSecurityException{
+        refreshColumn("A");
+        List<List<Object>> l = contentMap.get("Item");
+        return l.get(1).get(0).toString();
+    }
+
+
+    //note: get(a).get(b): a row, b column
+
+    /**
+     * Get the column name and content given the character of the column
+     * @param s the character of the column
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    public void refreshColumn(String s) throws IOException, GeneralSecurityException{
+        String sheetname = getSheetTitles().get(0);
+        String range = sheetname + "!"+s+":"+s;
+        ValueRange response = GoogleAPIUtils.getSheetsService().spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        columnMap.put(values.get(0).get(0).toString(),s);
+        contentMap.put(values.get(0).get(0).toString(),values);
+    }
+
 
     /**
      * Constructs a new VASpreadsheet object.
