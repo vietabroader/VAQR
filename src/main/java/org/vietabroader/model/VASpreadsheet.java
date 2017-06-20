d5464e6169e8748f82ce52a3480aea374dc1c54c
Cuong Nguyen <ctring23@gmail.com>
2017-06-20 00:15:50 -0700

    Reorganized and improved design of VASpreadsheet

--- src/main/java/org/vietabroader/model/VASpreadsheet.java
+++ src/main/java/org/vietabroader/model/VASpreadsheet.java
@@ -3,6 +3,7 @@ package org.vietabroader.model;
 import com.google.api.services.sheets.v4.model.Sheet;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.api.services.sheets.v4.model.ValueRange;
+import com.sun.istack.internal.Nullable;
 import org.vietabroader.GoogleAPIUtils;
 
 import java.io.IOException;
@@ -17,82 +18,34 @@ import java.util.List;
  */
 public class VASpreadsheet {
 
-
+    // Spreadsheet data
     private String spreadsheetId;
     private Spreadsheet spreadSheet;
-    private int fromRow;
-    private int toRow;
-    private HashMap<String,String> columnNameToChar;
-    private HashMap<String,List<Object>> cachedColumns;
 
-    /**
-     * Init the maps
-     */
-    public void init(){
-        columnNameToChar = new HashMap<String, String>();
-        cachedColumns = new HashMap<String,List<Object>>();
-    }
-
-    /**
-     * Set the column
-     * @param columnName the name of the column
-     * @param character the character of the column
-     */
-     public void setColumnChar(String columnName, String character){
-         columnNameToChar.put(columnName,character);
-     }
-
-    /**
-     * Set the row
-     * @param rowStart the first row
-     * @param rowEnd the last row
-     */
-     public void setRow(int rowStart, int rowEnd){
-         fromRow = rowStart;
-         toRow = rowEnd;
-     }
-
-    /**
-     * Read first key from the Item column for testing the refreshColumn method
-     * @param sheetName name of the sheet
-     * @param col name of the column
-     * @return the first key in the column
-     * @throws IOException
-     * @throws GeneralSecurityException
-     */
-    public List<Object> readCol(String sheetName, String col) throws IOException, GeneralSecurityException{
-        refreshColumn(sheetName,col);
-        return cachedColumns.get(col);
-    }
-
-
-    //note: get(a).get(b): a row, b column
-
-    /**
-     * Get the column name and content given the name of the column
-     * @param col the character of the column
-     * @throws IOException
-     * @throws GeneralSecurityException
-     */
-    public void refreshColumn(String sheetName, String col) throws IOException, GeneralSecurityException{
-        String colChar = columnNameToChar.get(col);
-        String range = sheetName + "!" + colChar + fromRow + ":" + colChar + toRow;
-        ValueRange response = GoogleAPIUtils.getSheetsService().spreadsheets().values()
-                .get(spreadsheetId, range)
-                .setMajorDimension("COLUMNS")
-                .execute();
-        List<List<Object>> values = response.getValues();
-        cachedColumns.put(col, values.get(0));
-    }
-
+    // Sheet data
+    private int fromRow = 1;
+    private int toRow = 1;
+    private String sheetName;
+    private HashMap<String, String> columnNameToChar = new HashMap<>();
+    private HashMap<String, List<Object>> cachedColumns = new HashMap<>();
 
     /**
      * Constructs a new VASpreadsheet object.
      * @param spreadsheetId id of the spreadsheet. This value can be obtained from the URL to the
      *                      spreadsheet.
      */
-    public VASpreadsheet(String spreadsheetId) throws IOException, GeneralSecurityException {
+    public VASpreadsheet(String spreadsheetId) {
         this.spreadsheetId = spreadsheetId;
+    }
+
+    /**
+     * Connect to the spreadsheet and download metadata. This method needs to be called before calling
+     * any of the sheet-related methods.
+     *
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    public void connect() throws IOException, GeneralSecurityException {
         this.spreadSheet = GoogleAPIUtils.getSheetsService().spreadsheets()
                 .get(spreadsheetId)
                 .execute();
@@ -115,4 +68,73 @@ public class VASpreadsheet {
         }
         return titles;
     }
+
+    /**
+     * Set a name for a column
+     * @param columnName the name of the column
+     * @param character the character of the column
+     */
+     public VASpreadsheet setColumnChar(String columnName, String character){
+         columnNameToChar.put(columnName,character);
+         return this;
+     }
+
+    /**
+     * Set the starting and ending row of this cache
+     * @param rowStart the first row
+     * @param rowEnd the last row
+     */
+     public VASpreadsheet setRow(int rowStart, int rowEnd){
+         fromRow = rowStart;
+         toRow = rowEnd;
+         return this;
+     }
+
+     public VASpreadsheet setSheetName(String sheetName) {
+         this.sheetName = sheetName;
+         return this;
+     }
+
+    /**
+     * Get the column name and content given the name of the column
+     *
+     * @param col the character of the column
+     * @return True if the specified column exists. False otherwise.
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    public boolean refreshOneColumn(String col) throws IOException, GeneralSecurityException {
+        String colChar = columnNameToChar.get(col);
+        if (colChar == null) {
+            return false;
+        }
+        String range = sheetName + "!" + colChar + fromRow + ":" + colChar + toRow;
+        ValueRange response = GoogleAPIUtils.getSheetsService().spreadsheets().values()
+                .get(spreadsheetId, range)
+                .setMajorDimension("COLUMNS")
+                .execute();
+
+        List<List<Object>> values = response.getValues();
+        cachedColumns.put(col, values.get(0));
+        return true;
+    }
+
+    public void refreshAllColumns() throws IOException, GeneralSecurityException {
+        for (String key : columnNameToChar.keySet()) {
+            refreshOneColumn(key);
+        }
+    }
+
+    /**
+     * Read a named column in this cache
+     * @param col name of the column
+     * @return items in specified column. Null if there is no such column
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    @Nullable
+    public List<Object> readCol(String col) throws IOException, GeneralSecurityException {
+        return cachedColumns.get(col);
+    }
+
 }
--- src/test/java/VASpreadsheetTest.java
+++ src/test/java/VASpreadsheetTest.java
@@ -4,8 +4,10 @@ import org.vietabroader.model.VASpreadsheet;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class VASpreadsheetTest {
 
@@ -15,38 +17,64 @@ public class VASpreadsheetTest {
     @Test
     public void testConstructVASpreadsheet() throws IOException, GeneralSecurityException {
         VASpreadsheet spreadsheet = new VASpreadsheet(TEST_SPREADSHEET_ID);
+        spreadsheet.connect();
         Assert.assertEquals("Test Spreadsheet", spreadsheet.getSpreadsheetTitle());
     }
 
     @Test(expected = IOException.class)
     public void testFailToConstructVASpreadsheet() throws IOException, GeneralSecurityException {
         VASpreadsheet spreadsheet = new VASpreadsheet("XXXXX");
+        spreadsheet.connect();
     }
 
     @Test
     public void testGetSheetTitles() throws IOException, GeneralSecurityException {
         VASpreadsheet spreadsheet = new VASpreadsheet(TEST_SPREADSHEET_ID);
+        spreadsheet.connect();
         List<String> titles = spreadsheet.getSheetTitles();
         Assert.assertTrue(titles.contains("Receipt"));
         Assert.assertTrue(titles.contains("Language"));
     }
 
+    @Test
+    public void testReadOneColumn() throws IOException, GeneralSecurityException {
+        VASpreadsheet spreadsheet = new VASpreadsheet(TEST_SPREADSHEET_ID);
+        spreadsheet.connect();
+
+        String ITEM = "Item";
+        spreadsheet.setSheetName("Receipt")
+                    .setColumnChar(ITEM, "A")
+                    .setRow(2, 4)
+                    .refreshOneColumn(ITEM);
+
+        List<Object> itemList = spreadsheet.readCol("Item");
+        assertList(itemList, "Book", "Laptop", "Desk");
+    }
 
     @Test
-    public void testReadKey() throws IOException, GeneralSecurityException{
+    public void testReadMultipleColumn() throws IOException, GeneralSecurityException {
         VASpreadsheet spreadsheet = new VASpreadsheet(TEST_SPREADSHEET_ID);
-        spreadsheet.init();
-        spreadsheet.setColumnChar("Item","A");
-        spreadsheet.setRow(2,4);
-        List<Object> list1 = spreadsheet.readCol("Receipt","Item");
-        List<Object> list2 = new ArrayList<Object>();
-        list2.add("Book");
-        list2.add("Laptop");
-        list2.add("Desk");
-        Assert.assertEquals(list1.size(),list2.size());
-        for (int i = 0; i < list1.size(); i++){
-            Assert.assertEquals(list1.get(i),list2.get(i));
-        }
+        spreadsheet.connect();
 
+        String PRICE = "Price";
+        String QUANTITY = "Quantity";
+        spreadsheet.setSheetName("Receipt")
+                .setColumnChar(PRICE, "B")
+                .setColumnChar(QUANTITY, "C")
+                .setRow(2, 4)
+                .refreshAllColumns();
+
+        List<Object> priceList = spreadsheet.readCol(PRICE);
+        List<Object> quantityList = spreadsheet.readCol(QUANTITY);
+        assertList(priceList, "$10", "$200", "$100");
+        assertList(quantityList, "2", "1", "10");
+    }
+
+    private void assertList(List<Object> actual, Object... expected) {
+        List<Object> expectedList = Arrays.asList(expected);
+        Assert.assertEquals(expectedList.size(), actual.size());
+        for (int i = 0; i < actual.size(); i++) {
+            Assert.assertEquals(expectedList.get(i), actual.get(i));
+        }
     }
 }
